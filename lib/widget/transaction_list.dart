3721846bfeebaf78426cacee87d41f1d5be3b50e
rofigates <rofigates95@gmail.com>
2020-06-14 01:35:51 +0700

    first commit

---
+++ lib/widget/transaction_list.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+import '../models/transaction.dart';
+
+class Transactionlist extends StatelessWidget {
+  final List<Transaction> transactions;
+
+  Transactionlist(this.transactions);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 300,
+        child: ListView.builder(
+          itemBuilder: (context, index) {
+            return Card(
+              child: Row(
+                children: <Widget>[
+                  Container(
+                    margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.purple[200], width: 2),
+                    ),
+                    padding: EdgeInsets.all(10),
+                    child: Text(
+                      '\$${transactions[index].amount}',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                          color: Colors.purple),
+                    ),
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        transactions[index].title,
+                        style: TextStyle(
+                            fontSize: 16, fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        DateFormat.yMMMMd().format(transactions[index].date),
+                        style: TextStyle(color: Colors.grey),
+                      )
+                    ],
+                  )
+                ],
+              ),
+            );
+          },
+          itemCount: transactions.length,          
+        ),
+    );
+  }
+}
---
+++ lib/widget/user_transaction.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+import './new_transaction.dart';
+import './transaction_list.dart';
+import '../models/transaction.dart';
+
+class UserTransaction extends StatefulWidget {
+  @override
+  _UserTransactionState createState() => _UserTransactionState();
+}
+
+class _UserTransactionState extends State<UserTransaction> {
+  final List<Transaction> _userTransaction = [
+    Transaction(
+        id: 't1', title: 'New Shoes', amount: 69.99, date: DateTime.now()),
+    Transaction(
+        id: 't2',
+        title: 'Weekly Groceris',
+        amount: 29.99,
+        date: DateTime.now()),
+  ];
+
+  void _addNewTransaction(String txTitle, double txAmount) {
+    final newTx = Transaction(
+        title: txTitle,
+        amount: txAmount,
+        date: DateTime.now(),
+        id: DateTime.now().toString()
+      );
+
+      setState(() {
+        _userTransaction.add(newTx);
+      });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        NewTransaction(_addNewTransaction), 
+        Transactionlist(_userTransaction)],
+    );
+  }
+}
